b29403391653ffe4cd3353c7e51224adb975bed0
alexmaragia <maragialex@gmail.com>
2024-08-21 00:51:39 +0300

    react-hooks-react-router-routes-lab-v6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
-import "./index.css";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import React from "react";
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+function Actors() {
+
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+    .then((response) => response.json())
+    .then((data) => setActors(data))
+    .catch((error) => console.error("Error fetching actor data", error));
+  }, []);
+
+  const actorList = actors.map(actor => (
+    <article key={actor.id}>
+      <h2>{actor.name}</h2>
+      <ul>
+        {actor.movies.map((movie,index) => (
+          <li key={index}>{movie}</li>
+        ))}
+      </ul>
+    </article>
+  ))
+
+
+  return (
+    <>
+      <header>
+        <NavBar />
+        <h1>Actors Page</h1>
+      </header>
+      <main>
+        {actorList}
+      </main>
+    </>
+  );
+};
+
+export default Actors;
--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,13 +1,38 @@
+import React from "react";
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+    .then((response) => response.json())
+    .then((data) => setActors(data))
+    .catch((error) => console.error("Error fetching actor data", error));
+  }, []);
+
+  const actorList = actors.map(actor => (
+    <article key={actor.id}>
+      <h2>{actor.name}</h2>
+      <ul>
+        {actor.movies.map((movie,index) => (
+          <li key={index}>{movie}</li>
+        ))}
+      </ul>
+    </article>
+  ))
+
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
+        <h1>Actors Page</h1>
       </header>
       <main>
-        {/* Actor info here! */}
+        {actorList}
       </main>
     </>
   );
